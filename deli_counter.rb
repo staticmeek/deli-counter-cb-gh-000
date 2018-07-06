f084d5be5c47de201ef234cc3580a069ff039035
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 11:46:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.first
+    "Currently serving #{katz_deli.first}"
     katz_deli.shift
   end
 end
