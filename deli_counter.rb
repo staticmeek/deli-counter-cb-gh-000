dd556c057c7f0f4c018e4b2a21c6db1d78cf8623
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 11:04:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-  katz_deli.each do |name|
-    katz_deli.push("The line is currently: #{number}. #{name} ")
-    number += 1
+    katz_deli.each do |name|
+      katz_deli.push("The line is currently: #{number}. #{name} ")
+      number += 1
+    end
   end
-  end  
 end
