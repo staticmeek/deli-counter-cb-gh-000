f874a94574f1ff8385c029897efa430d6d23fa84
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 11:31:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,6 +9,6 @@ end
 def take_a_number(katz_deli, name)
   number = 1
   if katz_deli.empty?
-    katz_deli.push(name)
+    katz_deli.push("Wlecome, #{name}. You are number #{number} in line.")
   end
 end
