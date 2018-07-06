d2adf9c2722043e1b63639e106654a965ce11d3b
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 12:28:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -31,10 +31,8 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each do |name|
-      katz_deli.join(" #{number}. ")
-    puts "The line is currently: #{number}. #{name} "
-    number += 1
+    katz_deli.each_with_index do |name, index|
+      puts "The line is currently: #{index.to_i + 1}. #{name}"
     end
   end
 end
