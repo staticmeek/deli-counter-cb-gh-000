e985d1f1ae6e5a24fa34fbbd5db40d964de62b2c
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 11:51:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,6 @@
 # Write your code here.
 require 'pry'
 
-
 def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
@@ -9,4 +8,12 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
+end
+
+def take_a_number(katz_deli, name)
+  number = 1
+  if katz_deli.empty?
+    puts "Welcome, #{name}. You are number #{number} in line."
+    number += 1
+  end    
 end
